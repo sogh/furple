d73b00d617638a6c339c45fc7ab0bf2d5807a5cc
sogh <mnichols13@gmail.com>
2020-11-01 21:31:28 -0800

    Update mygame.py
    The game was a bit too easy with the fix as is.

--- mygame.py
+++ mygame.py
@@ -1,7 +1,7 @@
 import random 
 
 print("Beginning game...")
-if random.randint(0,9) == 9:
+if random.randint(0,9) == 13:
     print("Through sheer will, you managed to win.")
 else :
 	print("You lose! Try again.")
