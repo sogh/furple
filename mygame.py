5babd2616d00273e7a9e8a854830b95072478f65
sogh <mnichols13@gmail.com>
2020-11-09 18:35:02 -0800

    Fixing messed up merge!

--- mygame.py
+++ mygame.py
@@ -1,4 +1,4 @@
-import random
+import random, sys
 
 from engine.player import Player
 
@@ -8,26 +8,45 @@ print(f"You wake up to find yourself in the middle of dirt road, you walk straig
 print("The left path is very foggy and you cannot see farther then a few feet")
 print("The right path is blocked with thick vines but you might be able to squeeze in")
 
-if random.randint(1,2) == 1 :
-    deadlypath = "left"
-else :
-    deadlypath = "right"
-    
-while True:
-    direction = input("Do you want to go left or right? :")
-    if direction.lower() == "left" :
-        print("you went left")
-        break
-    elif direction.lower() == "right" :
-        print("you went right")
-        break
-    else :
-        print(f"{direction} is not a direction you can go to, please choose left or right")
-# player1.step(direction)
-print(f"Position: {player1.position.toString()}")
-if deadlypath == direction.lower():
+def deadlypath(lowdir):
+    print(f"you went {lowdir}")
     print("You hear a hissing sound, looks like you stepped on a snake's tail")
     print("The snake bites you and you die")
     print("Game over.")
-else :
+    sys.exit()
+
+def winpath(lowdir):
+    print(f"you went {lowdir}")
     print("Through sheer will, you managed to win.")
+    sys.exit()
+
+killjester_really = False
+def killjester(lowdir):
+    global killjester_really
+    if not killjester_really:
+        print("Jingle is our friend.  She is here to help us.")
+        killjester_really = True
+    else:
+        print("Jingle is dead.")
+        print("Game over.")
+        print("Let's take another call.")
+        sys.exit()
+
+dirs = {
+    "kill jester": killjester,
+    "killjester": killjester,
+}
+if random.randint(1,2) == 1 :
+    dirs["left"] = deadlypath
+    dirs["right"] = winpath
+else :
+    dirs["right"] = deadlypath
+    dirs["left"] = winpath
+
+while True:
+    direction = input("Do you want to go left or right? :")
+    lowdir = direction.lower()
+    if lowdir in dirs:
+        dirs[lowdir](lowdir)
+    else:
+        print(f"{direction} is not a direction you can go to, please choose left or right")
