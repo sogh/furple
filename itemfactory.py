718af8bbe0f56219712cd2179a147444a946429d
Phezygg <Bojikov@gmail.com>
2020-12-12 20:48:29 -0500

    Adjusted itemfactory per overlord request

--- itemfactory.py
+++ itemfactory.py
@@ -1,25 +1,9 @@
 from engine.worldmap import WorldMap
 from random import randint, choice
 
-def initial_item_populate(new_world):
-
+def initial_item_populate(new_world, north_limit = 1, south_limit = -1, east_limit = 1, west_limit = -1):
     possible_items = ['a tree', 'a rock', 'a stream', 'a boot', 'some grass', 'some mushrooms', 'a cloud', 'crumpled paper', 'a fallen tree limb']
-    north_limit = 1
-    south_limit = -1
-    east_limit = 1
-    west_limit = -1
-    lastitem = randint(1,7)
-    x_list = []
-    y_list = []
+    max_initial_items = 7
 
-    #for x in range(west_limit - 1, east_limit + 1):
-    #    x_list.append(x)
-
-    #for y in range(south_limit - 1, north_limit + 1):
-    #    y_list.append(y)
-
-    x_list = list(range(west_limit, east_limit + 1))
-    y_list = list(range(south_limit, north_limit + 1))
-
-    for item in range(lastitem):
-        new_world.AddItemAt(choice(x_list), choice(y_list), choice(possible_items))
+    for _ in range(randint(1,max_initial_items)):
+        new_world.AddItemAt(choice(list(range(west_limit, east_limit + 1))), choice(list(range(south_limit, north_limit + 1))), choice(possible_items))
