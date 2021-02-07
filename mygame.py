9d7b41c5a138a6c4e2496139ece88a6273bb3ae9
sogh <mnichols13@gmail.com>
2021-02-07 13:20:53 -0800

    Fix a couple of bugs with input cmds

--- mygame.py
+++ mygame.py
@@ -16,8 +16,11 @@ class MyGame:
         self.look_commands = ['look', 'search', 'investigate']
         self.npc_commands = ['greet']
         self.quit_commands = ['quit']
+        self.nothing_commands = ['wait', 'nothing']
 
-        self.all_commands = self.info_commands + self.player_move_commands + self.fart_commands + self.quit_commands + self.look_commands + self.npc_commands
+        self.all_commands = (self.info_commands + self.player_move_commands
+            + self.fart_commands + self.quit_commands + self.look_commands 
+            + self.npc_commands + self.nothing_commands)
 
         self.worldmap = worldmap
         self.sim = sim
@@ -38,43 +41,44 @@ class MyGame:
 
 
     def Step(self):
-        self.graphics.RenderText(f"Position: {self.player1.position.toString()}")
-        self.graphics.RenderText("Move commands: n,s,e,w")
-        self.graphics.RenderText(self.sun.toString())
-        self.graphics.RenderText(self.worldmap.GetLocationDescription(self.player1.position.x, self.player1.position.y))
-        self.graphics.RenderText(self.player1.PlayerStatus())
-        
         # Every Loop execute one simulation tick.
         self.sim.Update()
 
         # Take player input
         if len(self.input_queue) > 0:
             cmd = self.input_queue.popleft()
-        else:
-            cmd = ''
-        lowcmd = cmd.lower()
-        if lowcmd in self.player_move_commands:
-            self.player1.move(lowcmd)
-        elif lowcmd in self.info_commands:
-            self.graphics.RenderText("All possible commands:")
-            self.graphics.RenderText(self.all_commands)
-        elif lowcmd in self.fart_commands:
-            self.graphics.RenderText("You fart. It smells.")
-        elif lowcmd in self.quit_commands:
-            return False
-        elif lowcmd in self.look_commands:
-            item_list_local = self.worldmap.GetItemDescriptions(self.player1.position.x, self.player1.position.y)
-            for item in item_list_local:
-                self.graphics.RenderText(f"You see {item}.")
-            #npc_list_local = 
-            for npc in self.worldmap.GetNPCDescriptions(self.player1.position.x, self.player1.position.y):
-                self.graphics.RenderText(f"You see {npc.title()}.")
-        elif lowcmd in self.npc_commands:
-            for greeting in self.worldmap.greetnpcs(self.player1.position.x, self.player1.position.y):
-                self.graphics.RenderText(greeting)
-        else:
-            self.graphics.RenderText(f"{cmd} is not a valid command.")
+            lowcmd = cmd.lower()
+            if lowcmd in self.player_move_commands:
+                self.player1.move(lowcmd)
+            elif lowcmd in self.info_commands:
+                self.graphics.RenderText("All possible commands:")
+                self.graphics.RenderText(self.all_commands)
+            elif lowcmd in self.fart_commands:
+                self.graphics.RenderText("You fart. It smells.")
+            elif lowcmd in self.quit_commands:
+                return False
+            elif lowcmd in self.look_commands:
+                item_list_local = self.worldmap.GetItemDescriptions(self.player1.position.x, self.player1.position.y)
+                for item in item_list_local:
+                    self.graphics.RenderText(f"You see {item}.")
+                #npc_list_local = 
+                for npc in self.worldmap.GetNPCDescriptions(self.player1.position.x, self.player1.position.y):
+                    self.graphics.RenderText(f"You see {npc.title()}.")
+            elif lowcmd in self.npc_commands:
+                for greeting in self.worldmap.greetnpcs(self.player1.position.x, self.player1.position.y):
+                    self.graphics.RenderText(greeting)
+            elif lowcmd in self.nothing_commands:
+                pass
+            else:
+                self.graphics.RenderText(f"{cmd} is not a valid command.")
         
+        self.graphics.RenderText(f"Position: {self.player1.position.toString()}")
+        self.graphics.RenderText("Move commands: n,s,e,w")
+        self.graphics.RenderText(self.sun.toString())
+        self.graphics.RenderText(self.worldmap.GetLocationDescription(self.player1.position.x, self.player1.position.y))
+        self.graphics.RenderText(self.player1.PlayerStatus())
+
+
         # Render all the graphics
         self.graphics.Render()
 
@@ -84,7 +88,7 @@ class MyGame:
     def Run(self):
         # Main game loop
         while self.Step():
-            self.input_queue.append(input("Enter command: "))
+            self.input_queue.appendleft(input("Enter command: "))
 
 
 if __name__ == '__main__':
